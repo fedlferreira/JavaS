0eb1bdf68847fcfaa5aa9d1e8b3dbb87f926a2a1
Felipe D L Ferreira <ffylepe@hotmail.com>
2024-07-11 20:07:50 -0300

    array

---
+++ aula15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`nosso vetor é o ${num.length}`)
+console.log(`o primeiro numero é ${num[0]}`)
+let pos = num.indexOf(5)
+if (pos == -1) {
+    console.log('o valor não foi encontrado!')} else {
+console.log(`o valor 4 está na posição${pos}`)}
+
+
+
+
